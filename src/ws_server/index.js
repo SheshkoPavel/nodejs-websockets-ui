12488a5771d737d3c558e644902a6134570a62f9
Pavel Sheshko <psheshko@twelvedevs.com>
2023-07-07 15:52:02 +0300

    feat: firs ws request

--- src/ws_server/index.js
+++ src/ws_server/index.js
@@ -8,22 +8,43 @@ export const wsServer = new WebSocketServer({ port });
 export const wsCallback = (ws) => {
     console.log('Connected <-------', ws);
 
-    ws.send('Hello noob 2!')
+    ws.send(JSON.stringify('Hello noob!'))
 
     ws.on('message', (message) => {
-        console.log(message);
-        ws.send(typeof message)
 
-        const parsedMSG = JSON.parse(JSON.stringify(message));
+        // You need to stringify the json, not calling toString
+        // var buf = Buffer.from(JSON.stringify(obj);
+
+        // And for converting string to json obj
+        // var temp = JSON.parse(buf.toString());
+
+        const parsedMSG = JSON.parse(message.toString());
         console.log('parsedMSG <-------', parsedMSG);
-        ws.send(typeof parsedMSG)
 
-        if (message === 'hi') {
-            ws.send(`Hello, my dear`);
-        } else if (message == 'bye') {
-            ws.send(`So what?`);
-        } else {
-            ws.send(`Your message is ${message}`);
+        const obj = {
+            type: "reg",
+            data:
+                {
+                    name: '11111',
+                    index: 1,
+                    error: false,
+                    errorText: 'Error string',
+                },
+            id: 0,
+        };
+
+        obj.data = JSON.stringify(obj.data);
+
+        if (parsedMSG.type === 'reg') {
+            ws.send(JSON.stringify(obj));
         }
+
+        // if (message === 'hi') {
+        //     ws.send(`Hello, my dear`);
+        // } else if (message == 'bye') {
+        //     ws.send(`So what?`);
+        // } else {
+        //     ws.send(JSON.stringify(message));
+        // }
     })
 };
